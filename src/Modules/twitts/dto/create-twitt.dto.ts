da55f5e1de7ead638953792179db1bd0cba54379
jdeca <jdec@unal.edu.co>
2023-03-07 18:29:20 -0500

    Add Modules & dto

---
+++ src/Modules/twitts/dto/create-twitt.dto.ts
@@ -0,0 +1,3 @@
+export class CreateTwittDto {
+  readonly message: string;
+}
---
+++ src/Modules/twitts/dto/update-twitt.dto.ts
@@ -0,0 +1,5 @@
+import { IsString } from 'class-validator';
+export class UpdateTwittDto {
+  @IsString()
+  readonly message: string;
+}
